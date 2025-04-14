a89425e7a6b34f96863caa0eb3aa864a9d0721eb
nafisahsyh <nafisahsayyidah4@gmail.com>
2025-04-14 20:13:08 +0800

    perbaiki error

--- app/src/main/java/com/example/infak/MainActivity.java
+++ app/src/main/java/com/example/infak/MainActivity.java
@@ -1,24 +1,23 @@
 package com.example.infak;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DURATION = 3000; // 3 detik
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.activity_main); // ini layout splash-nya
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish(); // supaya splash gak bisa di-back
+        }, SPLASH_DURATION);
     }
 }
